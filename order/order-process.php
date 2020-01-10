b52b97fa316c659b1e1b05c05f8e8f0770498874
EliasH94 <54803079+EliasH94@users.noreply.github.com>
2020-01-10 10:10:44 +0100

    Update

--- order/order-process.php
+++ order/order-process.php
@@ -4,14 +4,31 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'):// Hämta och rensa data från POST-Arr
     $product_id= htmlspecialchars($_POST['product_id']);
     $price= htmlspecialchars($_POST['price']);
     $email= htmlspecialchars($_POST['email']);
+    $name= htmlspecialchars($_POST['name']);
+    $number= htmlspecialchars($_POST['number']);
+    $address= htmlspecialchars($_POST['address']);
+    
 
 require_once 'db.php';
-$sql= "SELECT * FROM customer WHERE email LIKE '%email%'";
+$sql= "SELECT * FROM customer WHERE email LIKE '$email'";
 $stmt= $db->prepare($sql);
 $stmt->bindParam('email', $email, PDO::PARAM_INT);
 $stmt->execute();
-echo "hello";
+
 if($stmt->rowCount() == 0) {
+    $stmt = $db->prepare("INSERT INTO customer (name, number, email, address) 
+VALUES (:name, :number, :email, :address)");
+$stmt->bindParam(':name', $name); 
+$stmt->bindParam(':number' , $number);
+$stmt->bindParam(':email' , $email);
+$stmt->bindParam(':address' , $address);
+
+$namn = $_POST['name']; 
+$telefonnummer = $_POST['number'];
+$epostadress = $_POST['email'];
+$leveransadress = $_POST['address'];
+
+$stmt->execute();
     $message= "<div class='alert alert-warning'>OBS! Felaktigt kundnummer!</div>";
     echo "$message";
     } 
@@ -20,6 +37,13 @@ if($stmt->rowCount() == 0) {
     $id = $row['id'];
     $message= "<div class='alert alert-success'><h3>Tack $id</h3>";
     echo "$message";
+
+    $sql= "INSERT INTO orders (film, customer) VALUES (:film, :customer)";
+    $stmt= $db->prepare($sql);
+    $stmt->bindParam(':film', $film_id, PDO::PARAM_INT);
+    $stmt->bindParam(':customer', $customer_id, PDO::PARAM_INT);
+    $stmt->execute();
+
 }
 
 endif;
